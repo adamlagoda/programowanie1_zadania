68147f592bb924a7863cbd9bf7e6eb5a64b48c65
Adam Łagoda <adam.lagoda@protonmail.com>
2019-11-11 21:49:01 +0100

    First predicate template

---
+++ src/main/java/predicate/ConstainsPolishCharactersPredicate.java
@@ -0,0 +1,11 @@
+package predicate;
+
+import java.util.function.Predicate;
+
+public class ConstainsPolishCharactersPredicate implements Predicate<String> {
+
+    @Override
+    public boolean test(String s) {
+        return false;
+    }
+}
---
+++ src/test/java/predicate/PredicateTest.java
@@ -0,0 +1,52 @@
+package predicate;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Predicate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PredicateTest {
+
+    private final String containingPolishChars = "Dżawa";
+    private final String nonContainingPolishChars = "Java";
+
+    @Test
+    @Disabled
+    public void containsPolishCharactersTestLambda() {
+        Predicate<String> containsPolishCharacters;
+
+        assertTrue(containsPolishCharacters.test(containingPolishChars));
+        assertFalse(containsPolishCharacters.test(nonContainingPolishChars));
+    }
+
+    @Test
+    @Disabled
+    public void containsPolishCharactersTestAnonymousClass() {
+        Predicate<String> containsPolishCharacters;
+
+
+        assertTrue(containsPolishCharacters.test(containingPolishChars));
+        assertFalse(containsPolishCharacters.test(nonContainingPolishChars));
+    }
+
+    @Test
+    @Disabled
+    public void containsPolishCharactersTestImplementation() {
+        Predicate<String> containsPolishCharacters;
+
+        assertTrue(containsPolishCharacters.test(containingPolishChars));
+        assertFalse(containsPolishCharacters.test(nonContainingPolishChars));
+    }
+
+    @Test
+    @Disabled
+    public void containsPolishCharactersTest() {
+        Predicate<String> containsPolishCharacters;
+
+        assertTrue(containsPolishCharacters.test(containingPolishChars));
+        assertFalse(containsPolishCharacters.test(nonContainingPolishChars));
+    }
+}
