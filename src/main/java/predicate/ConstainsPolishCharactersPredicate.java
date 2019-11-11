d293ec4982fb6ba502fcd9b51beae25920b45ede
Adam Łagoda <adam.lagoda@protonmail.com>
2019-11-11 22:13:35 +0100

    Solutions to first predicate task

--- src/main/java/predicate/ConstainsPolishCharactersPredicate.java
+++ src/main/java/predicate/ConstainsPolishCharactersPredicate.java
@@ -4,8 +4,16 @@ import java.util.function.Predicate;
 
 public class ConstainsPolishCharactersPredicate implements Predicate<String> {
 
+    private final char[] polishCharacters = {'ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ż', 'ź'};
+
     @Override
     public boolean test(String s) {
-        return false;
+        return s.chars()
+                .anyMatch(c -> {
+                    for (char polishChar : polishCharacters)
+                        if (polishChar == c)
+                            return true;
+                    return false;
+                });
     }
 }
--- src/test/java/predicate/PredicateTest.java
+++ src/test/java/predicate/PredicateTest.java
@@ -3,9 +3,7 @@ package predicate;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,11 +14,19 @@ public class PredicateTest {
     private final String nonContainingPolishChars = "Java";
     private final User userWithOnePhone = new User();
     private final User userWithMultiplePhones = new User();
+    private final char[] polishCharacters = {'ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ż', 'ź'};
 
     @Test
     @Disabled
     public void containsPolishCharactersTestLambda() {
-        Predicate<String> containsPolishCharacters = null;
+        Predicate<String> containsPolishCharacters = s ->
+                s.chars()
+                        .anyMatch(c -> {
+                            for (char polishChar : polishCharacters)
+                                if (c == polishChar)
+                                    return true;
+                            return false;
+                        });
 
         assertTrue(containsPolishCharacters.test(containingPolishChars));
         assertFalse(containsPolishCharacters.test(nonContainingPolishChars));
@@ -29,9 +35,18 @@ public class PredicateTest {
     @Test
     @Disabled
     public void containsPolishCharactersTestAnonymousClass() {
-        Predicate<String> containsPolishCharacters = null;
-
-
+        Predicate<String> containsPolishCharacters = new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return s.chars()
+                        .anyMatch(c -> {
+                            for (char polishChar : polishCharacters)
+                                if (polishChar == c)
+                                    return true;
+                            return false;
+                        });
+            }
+        };
         assertTrue(containsPolishCharacters.test(containingPolishChars));
         assertFalse(containsPolishCharacters.test(nonContainingPolishChars));
     }
@@ -39,7 +54,7 @@ public class PredicateTest {
     @Test
     @Disabled
     public void containsPolishCharactersTestImplementation() {
-        Predicate<String> containsPolishCharacters = null;
+        Predicate<String> containsPolishCharacters = new ConstainsPolishCharactersPredicate();
 
         assertTrue(containsPolishCharacters.test(containingPolishChars));
         assertFalse(containsPolishCharacters.test(nonContainingPolishChars));
